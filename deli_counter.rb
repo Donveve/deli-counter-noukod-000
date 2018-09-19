df3c4ff181047ac05c73d1f8fa57ea56331eeeba
Veniel Dorestave <veniel@noukod.com>
2018-09-19 16:22:02 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,4 +1,4 @@
-def line(array) # this was the one I figured out
+def line(array)t
   if array.length >= 1
     nuarray = []
     counter = 1
@@ -14,21 +14,16 @@ end
 
 line(katz_deli)
 
-def line_simple(array) # this one follows the same mechanics as learn.co
+def line_simple(array) 
   current_line = "The simple line is currently:"
   array.each.with_index(1) do |value, indexemus|
-  # "each.with_index" is the method...must use "index"
+  
     current_line << " #{indexemus}. #{value},"
-    # "indexemus" is used to illustrate this variable can be different from "index"
   end
   puts current_line
 end
 
 line_simple(katz_deli)
-#[:foo, :bar, :baz].each.with_index(1) do |value, index|
-#    puts "#{index}: #{value}"
-#end
-
 
 
 def take_a_number(line, new_person)
